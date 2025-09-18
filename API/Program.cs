78a9a2cc2d664923b990cfec1a72de5c7e2eeb77
revlienn <revnathalia@gmail.com>
2025-09-18 11:49:15 +0100

    1442

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
