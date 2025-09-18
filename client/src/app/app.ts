f4543897b09d90a677aff63c3f0cd002e07ed5c3
revlienn <revnathalia@gmail.com>
2025-09-18 12:10:00 +0100

    1445

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,21 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = "client";
+  protected readonly title = signal('client');
   private http=inject(HttpClient);
-  protected members=signal<any>([]);
+  protected users=signal<any>([]);
 
   ngOnInit():void{
     this.http.get('https://localhost:5001/api/members').subscribe({
-      next: (response) => this.members.set(response),
+      next: (response) => this.users.set(response),
       error: (error) => console.log(error),
       complete: () => console.log('HTTP Request Complete'),
     });
