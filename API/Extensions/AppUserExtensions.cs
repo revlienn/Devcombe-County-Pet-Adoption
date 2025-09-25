f40834a5ebd0c969cabc88121be2cf84356f217b
revlienn <revnathalia@gmail.com>
2025-09-25 13:55:19 +0100

    1487

--- API/Extensions/AppUserExtensions.cs
+++ API/Extensions/AppUserExtensions.cs
@@ -17,7 +17,8 @@ namespace API.Extensions
                 Id = user.Id,
                 DisplayName = user.DisplayName,
                 Email = user.Email,
-                Token = tokenService.CreateToken(user)
+                Token = tokenService.CreateToken(user),
+                ImageUrl = user.ImageUrl
             };
         }
     }
