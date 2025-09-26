abdafad0e467fb8898adc5fe6582c7b0baa1507d
revlienn <revnathalia@gmail.com>
2025-09-26 11:15:41 +0100

    1499

--- client/src/shared/card/card.ts
+++ client/src/shared/card/card.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
-import { Member } from '../types/member';
-import { Pet } from '../types/pet';
 import { RouterLink } from '@angular/router';
+import { Member } from '../../types/member';
+import { Pet } from '../../types/pet';
 
 @Component({
   selector: 'app-card',
