918c2dfe2480ffc3e9a019e9672a659f8c720043
revlienn <revnathalia@gmail.com>
2025-09-29 11:59:14 +0100

    1509

--- client/src/members/member-details/member-details.ts
+++ client/src/members/member-details/member-details.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MemberService } from '../../services/member-service';
 import { filter, Observable } from 'rxjs';
 import { Member } from '../../types/member';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
+import { AccountService } from '../../services/account-service';
 
 @Component({
   selector: 'app-member-details',
@@ -13,17 +14,23 @@ import { AsyncPipe } from '@angular/common';
 })
 export class MemberDetails {
   private memberService=inject(MemberService);
+  private accountService=inject(AccountService);
   private activeRoute=inject(ActivatedRoute);
   private router=inject(Router);
   protected member=signal<Member|undefined>(undefined);
   protected title=signal<string|undefined>('Profile');
+
+  protected isCurrentUser=computed(()=>{
+    return this.accountService.currentUser()?.id===this.activeRoute.snapshot.paramMap.get('id');
+  })
 
   constructor() {
     this.activeRoute.data.subscribe({
       next:data=>this.member.set(data['member'])
     })
     this.title.set(this.activeRoute.firstChild?.snapshot?.title);
-    console.log(this.activeRoute);
+
+    console.log('ACTIVE ROUTE: ',this.activeRoute);
 
     this.router.events.pipe(filter(ev=>ev instanceof NavigationEnd)).subscribe({
       next:()=>
