eb3e88b5e88473b73e38fae6d5e32ac461234d28
revlienn <revnathalia@gmail.com>
2025-09-29 10:51:54 +0100

    1507

--- client/src/navbar/navbar.ts
+++ client/src/navbar/navbar.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../services/toast-service';
+import { themes } from '../app/theme';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +11,28 @@ import { ToastService } from '../services/toast-service';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit{
   protected accountService=inject(AccountService);
   private toastService=inject(ToastService);
   private router=inject(Router);
   protected creds:any={};
+
+  protected selectedTheme=signal<string>(localStorage.getItem('theme')||'cmyk');
+  protected themes=themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectTheme(theme:string){
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme',theme);
+
+    document.documentElement.setAttribute('data-theme',theme);
+    
+    const el = document.activeElement as HTMLDivElement;
+    if (el) el.blur()
+  }
 
   login(){
     this.accountService.login(this.creds).subscribe({
